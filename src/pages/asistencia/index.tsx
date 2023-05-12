6620e83eb9309cf3f279ebb561a1a791f70ccc39
Annel <hdzgaeta@gmail.com>
2023-05-12 16:04:02 -0600

    update filter of name

--- src/pages/asistencia/index.tsx
+++ src/pages/asistencia/index.tsx
@@ -1,12 +1,13 @@
-import React from 'react';
 import { Box,Stack} from '@chakra-ui/react';
 import { useState } from "react";
-import { DataTable } from "primereact/datatable";
+import { DataTable } from 'primereact/datatable';
+import { Button } from 'primereact/button';
 import { Column } from "primereact/column";
 import { InputText } from 'primereact/inputtext';
 import { FilterMatchMode } from 'primereact/api';
 import { useQuery} from "react-query";
 import {getSesiones} from 'services/api/Auth';
+import moment from 'moment';
 
 export default function Asistencia (){
   const { data: sesiones } = useQuery(["sesion"], getSesiones);
@@ -15,29 +16,30 @@ export default function Asistencia (){
   const onGlobalFilterChange1 = (e:any) => {
         setGlobalFilterValue1(e.target.value);
   }
-   
+
     return (
         <Stack w="80%" mx="auto" mt="10" spacing="5">
-            <DataTable   width="80%" 
-             header={
-              <Box display='flex' justifyContent='flex-start'>
-                <span className="p-input-icon-left">
-                  <i className="pi pi-search" />
-                  <InputText value={globalFilterValue1} onChange={onGlobalFilterChange1} placeholder="Filtrar por nombre del trabajador ..." />
-                </span>
-              </Box>
-            } 
-            filters={{
-              '': { value: globalFilterValue1, matchMode: FilterMatchMode.STARTS_WITH }
-            }}
-            value={ sesiones?.map((element: any) => {return(element)})}
-            >
-                <Column field="attributes.user.data.attributes.username" header="Nombre" />
-                <Column field="attributes.fecha_registro" header="Fecha" />
-                <Column field="attributes.hora_registro" header="Hora de entrada" />
-            </DataTable>
-            
-      </Stack> 
+          <DataTable   width="80%" 
+            header={
+            <Box display='flex' justifyContent='flex-start'>
+              <span className="p-input-icon-left">
+                <i className="pi pi-search" />
+                <InputText value={globalFilterValue1} onChange={onGlobalFilterChange1} placeholder="Filtrar por nombre ..." />
+              </span>
+            </Box>
+          } 
+          filters={{
+            'attributes.user.data.attributes.username': { value: globalFilterValue1, matchMode: FilterMatchMode.STARTS_WITH }
+          }}
+          value={ sesiones?.map((element: any) => {return(element)})}
+          >
+              <Column field="attributes.user.data.attributes.username" header="Nombre" />
+              <Column field="attributes.fecha_registro" header="Fecha" />
+              <Column header="Hora de entrada" body={(data:any) => {
+                  return moment(data.attributes.hora_registro, 'hhmm ').format('hh:mm a')
+              }}/>
+          </DataTable>
+        </Stack> 
        
         
     )
