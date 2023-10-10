c1e424206ac51b11cf1e142261c57e4e25a4c4a5
mariorocha2308 <virgoroch852@gmail.com>
2023-10-10 16:57:09 -0600

    feat: add sucursal select to session

--- src/pages/auth/WelcomenPage.tsx
+++ src/pages/auth/WelcomenPage.tsx
@@ -1,29 +1,49 @@
-import { Box, Button, Text, VStack } from '@chakra-ui/react'
+import { Box, Button, Text, VStack, useToast } from '@chakra-ui/react'
 
 import { useAuth } from 'hooks/useAuth';
 import { fullName } from 'types/User';
 import { CardWithAvatar } from 'components/CardWithAvatar'
 import { formatHour } from 'helpers/format';
 import { useNavigate } from 'react-router-dom';
+import { useQuery } from 'react-query';
+import { getSubsidiaries } from 'services/api/subsidiary';
+import { Dropdown } from 'primereact/dropdown';
+import { useState } from 'react';
 
 const WelcomenPage = () =>{
+
   const navigate = useNavigate();
+  const toast = useToast()
+  const [sucursal, setSucursal] = useState('')
   const auth = useAuth();
   const now = new Date();
 
+  const { data: subsidiaries } = useQuery(["subsidiaries"], getSubsidiaries)
+
   const handleContinue = () => {
+    if (!sucursal) {
+      toast({
+        title: 'seleccion una sucursal',
+        status: 'warning'
+      })
+      return;
+    }
+
+    localStorage.setItem('sucursal', JSON.stringify(sucursal))
+    setSucursal('')
     navigate('/dashboard');
   }
 
+  console.log(sucursal);
+
   return (
-    <VStack marginY="auto" marginX="auto" maxW="xl" spacing={8}>
+    <VStack marginY="auto" marginX="auto" maxW="xl" spacing={4}>
       <CardWithAvatar
         w="full"
         bg='brand.500'
         avatarProps={{
           backgroundColor: 'brand.600',
         }}
-        mb={5}
       >
         <VStack color="white" fontSize={30} spacing={10} alignItems="center" pt={5}>
           <Box>
@@ -37,6 +57,18 @@ const WelcomenPage = () =>{
           </Box>
         </VStack>
       </CardWithAvatar>
+
+      <Box display='flex' gap='0.5rem' width='100%' flexDirection='column'>
+        <label htmlFor="name">Sucursal</label>
+        <Dropdown style={{width: '100%'}} placeholder='Ingresa una sucursal' value={sucursal} inputId="dropdown" options={subsidiaries?.map((subsiduary: any) => {
+            return {
+              name: subsiduary.id,
+              value: subsiduary.attributes.nombre
+            }
+          })} 
+          onChange={(e) => setSucursal(e.target.value)} optionLabel="value" required
+        />
+      </Box>
 
       <Button 
         onClick={handleContinue}
