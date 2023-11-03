e48f4acb4b7cdbaa42314de631f24862a43cb1f7
Edgardo Hernandez <edgardo.hernandez@it-and-apps.mx>
2023-11-02 18:28:20 -0600

    estatus pedidos

--- src/pages/taskbox/distribution.tsx
+++ src/pages/taskbox/distribution.tsx
@@ -1,5 +1,5 @@
 // import { useState } from "react";
-import { Badge, Box, Text } from "@chakra-ui/react";
+import { Badge, Box, IconButton, Text } from "@chakra-ui/react";
 import { ListBox } from "primereact/listbox";
 // import { Button } from "primereact/button";
 // import RecieveArticle from "../../components/modals/ReceiveArticle";
@@ -7,7 +7,13 @@ import { IoMdCalendar } from "react-icons/io";
 import { useQuery } from "react-query";
 import { getOrderDistribution } from "services/api/orders";
 import moment from "moment";
-import { MdOutlineHomeWork, MdTrolley } from "react-icons/md";
+import {
+  MdDone,
+  MdDoneAll,
+  MdEdit,
+  MdOutlineHomeWork,
+  MdTrolley,
+} from "react-icons/md";
 
 const Distribution = () => {
   const { data: orders } = useQuery(["orders"], getOrderDistribution);
@@ -28,30 +34,64 @@ const Distribution = () => {
   const itemTemplate = (order: any) => {
     return (
       <Box w="100%">
-        <Box display='flex' alignItems='center' gap='2'>
-          <Text fontWeight="bold" fontSize='17'>{order?.attributes?.comentario}</Text>
-          <Badge colorScheme="red" marginBottom='1'>{order?.attributes?.estatus}</Badge>
-        </Box>
-        <Box mt='1.5'>
+        <Box display="flex" justifyContent="space-between" w="100%">
+          <Box mt="1.5">
+            <Box display="flex" alignItems="center" gap="3">
+              <Text fontWeight="bold" fontSize="17">
+                {order?.attributes?.comentario}
+              </Text>
+              <Badge
+                colorScheme={
+                  order?.attributes?.estatus === "pendiente"
+                    ? "yellow"
+                    : "green"
+                }
+                marginBottom="1"
+              >
+                {order?.attributes?.estatus}
+              </Badge>
+            </Box>
             <Box display="flex" alignItems="center" gap="2">
               <IoMdCalendar />
               <Text>{moment(order?.attributes?.createdAt).format("L")}</Text>
             </Box>
-          <Box display="flex" alignItems="center" gap="2">
-            <MdOutlineHomeWork/> 
-            <Text>{order?.attributes?.sucursal?.data?.attributes?.nombre}</Text>
-          </Box>
-          <Box display="flex" alignItems="center" gap="2">
-            <MdTrolley/>
-            <Text>{order?.attributes?.bodega?.data?.attributes?.nombre}</Text>
-          </Box>
-          {/* <Box display="flex" alignItems="center" marginRight="5">
+            <Box display="flex" alignItems="center" gap="2" >
+              <MdOutlineHomeWork />
+              <Text>
+                {order?.attributes?.sucursal?.data?.attributes?.nombre}
+              </Text>
+            </Box>
+            <Box display="flex" alignItems="center" gap="2">
+              <MdTrolley />
+              <Text>{order?.attributes?.bodega?.data?.attributes?.nombre}</Text>
+            </Box>
+
+            {/* <Box display="flex" alignItems="center" marginRight="5">
             <Button
               icon="pi pi-eye"
               className="p-button-rounded"
               onClick={() => OpenDialog(order)}
             ></Button>
           </Box> */}
+            <br />
+          </Box>
+          <Box
+            display="flex"
+            alignItems="center"
+            marginRight="5"
+            hidden={order?.attributes?.estatus === "pendiente" ? false : true}
+          >
+            <IconButton
+            data-pr-tooltip="Recibido"
+              aria-label="show dialog"
+              icon={<MdDone />}
+              borderRadius="full"
+              colorScheme="green"
+              onClick={() => {}}
+              fontSize="30px"
+              size="lg"
+            />
+          </Box>
         </Box>
       </Box>
     );
